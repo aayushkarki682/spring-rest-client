9a717f285c8ad74759bdbfbcaf8b21a24aea57a4
aayushkarki682 <ayush.karki12345@gmail.com>
2021-03-08 13:35:36 -0600

    added data models based on JSON schema

---
+++ src/main/java/com/ayushproject/springrestclient/model/Location.java
@@ -0,0 +1,12 @@
+package com.ayushproject.springrestclient.model;
+
+import java.io.Serializable;
+
+public class Location implements Serializable {
+
+    private String street;
+    private String city;
+    private String state;
+    private String postcode;
+
+}
